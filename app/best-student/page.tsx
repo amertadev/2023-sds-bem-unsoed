7ed964a1ff614e2ba90ccce9d6c2d88bcfcfdbb4
send0moka <sendomoka2@gmail.com>
2024-06-23 12:46:40 +0700

    feat: Update footer with new design and navigation links
    This commit updates the footer component to include a new design and navigation links. It adds a background color of #3a3c67, sets the text color to white with 90% opacity, and adjusts the font to font-display. The logo is updated to use a dark version, and the navigation links for "Best Student," "After Movie," "Staff SDS," and "Partner" are added. The copyright information is also included. This enhancement improves the visual appeal and usability of the footer section.

---
+++ app/best-student/page.tsx
@@ -0,0 +1,7 @@
+export default function BestStudent() {
+    return (
+        <div className="bg-red-400 w-full p-40">
+            <h1>Best Student</h1>
+        </div>
+    );
+}
--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,17 +1,22 @@
+import Link from "next/link";
+import { DockDemo } from "./dock";
+
 export default function Footer() {
   return (
-    <div className="absolute w-full px-5 py-5 text-center">
-      <p className="text-gray-500">
-        Made with 💖 by {" "}
-        <a
-          className="font-semibold text-gray-600 underline-offset-4 transition-colors hover:underline"
-          href="https://twitter.com/steventey"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Creative Media of Soedirman Digital School
-        </a>
-      </p>
+    <div className="bg-[#3a3c67] text-white/90 font-display absolute w-full px-5 py-10 lg:px-40">
+      <div className="flex flex-col md:flex-row justify-between ">
+        <div className="flex flex-col justify-center md:justify-start">
+          <img src="/assets/logo-dark.svg" alt="Soedirman Digital School - BEM UNSOED" width={300} height={30} />
+          <div className="flex flex-col my-8">
+            <Link href="/best-student">Best Student</Link>
+            <Link href="/after-movie">After Movie</Link>
+            <Link href="/staff-sds">Staff SDS</Link>
+            <Link href="/partner">Partner</Link>
+          </div>
+          <small>© 2024 Soedirman Digital School. All rights reserved.</small>
+        </div>
+        <DockDemo />
+      </div>
     </div>
   );
 }
