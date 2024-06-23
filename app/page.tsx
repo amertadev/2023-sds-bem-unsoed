3f35e1a966ce10b21428333ec0a75aad1951b54e
send0moka <sendomoka2@gmail.com>
2024-06-23 19:32:45 +0700

    chore: Update page.tsx to open link in new tab
    This commit modifies the page.tsx file to update the link to "https://bit.ly/GuideBookSDS2023" so that it opens in a new tab. By adding the "target="_blank"" attribute to the anchor tag, we ensure that the link opens in a separate browser tab, improving the user experience and preventing users from navigating away from the current page.

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default async function Home() {
     <>
       <ScrollHandler />
       <div className="z-10 w-full max-w-2xl px-5 xl:px-0">
-        <a href="https://bit.ly/GuideBookSDS2023"><TextBox /></a>
+        <a href="https://bit.ly/GuideBookSDS2023" target="_blank"><TextBox /></a>
         <h1
           className="animate-fade-up bg-gradient-to-br from-[#21223a] to-[#21223a86] bg-clip-text text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm [text-wrap:balance] md:text-7xl md:leading-[5rem]"
           style={{ animationDelay: "0.15s", animationFillMode: "forwards" }}
