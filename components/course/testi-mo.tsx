071ed5cc01f87c7cc335f79f208bdbb44a6a4c24
send0moka <sendomoka2@gmail.com>
2024-06-24 14:37:47 +0700

    refactor: Update TestiMO component with new testimonials
    This commit updates the TestiMO component to display new testimonials from different users. The previous testimonials are replaced with testimonials from Fitriana Nurbaiti, Isna Febriyana, and Regita Rahman. This modification improves the relevance and freshness of the displayed testimonials.

--- components/course/testi-mo.tsx
+++ components/course/testi-mo.tsx
@@ -4,25 +4,25 @@ export default function TestiMO() {
             <div className="p-2 border bg-white rounded-lg inline-block mb-2">
                 <span className="font-display opacity-70">Testimonials👩‍💻</span>
             </div>
-            <div className="grid md:grid-cols-3 lg:grid-cols-5 gap-10">
-                <div className="flex flex-col gap-2 rounded-lg border bg-background p-6 shadow-lg">
-                    <img src="/assets/testi-1.jpg" alt="testi-1" className="h-20 md:h-24 w-full lg:max-w-2xl object-cover rounded-lg" />
-                    <div className="md:ml-6 md:mt-6 flex flex-col items-center md:items-start">
-                        <h3 className="font-bold text-xl md:text-2xl text-tremor-content-strong">Amanda</h3>
+            <div className="flex gap-4">
+                <div className="flex gap-2 rounded-lg border bg-background p-6 shadow-lg">
+                    <img src="/assets/fitri.png" alt="testi-1" className="h-12 w-12 object-cover rounded-full" />
+                    <div>
+                        <p className="font-bold text-tremor-content-strong">Fitriana Nurbaiti</p>
                         <p className="text-tremor-content">Saya sangat senang bisa mengikuti Microsoft Office bersama Soedirman Digital School. Materi yang disampaikan sangat mudah dipahami dan bermanfaat. Terima kasih!</p>
                     </div>
                 </div>
-                <div className="flex flex-col gap-2 rounded-lg border bg-background p-6 shadow-lg">
-                    <img src="/assets/testi-2.jpg" alt="testi-2" className="h-20 md:h-24 w-full lg:max-w-2xl object-cover rounded-lg" />
-                    <div className="md:ml-6 md:mt-6 flex flex-col items-center md:items-start">
-                        <h3 className="font-bold text-xl md:text-2xl text-tremor-content-strong">Budi</h3>
+                <div className="flex gap-2 rounded-lg border bg-background p-6 shadow-lg">
+                    <img src="/assets/isna.png" alt="testi-1" className="h-12 w-12 object-cover rounded-full" />
+                    <div>
+                        <p className="font-bold text-tremor-content-strong">Isna Febriyana</p>
                         <p className="text-tremor-content">Saya sangat senang bisa mengikuti Microsoft Office bersama Soedirman Digital School. Materi yang disampaikan sangat mudah dipahami dan bermanfaat. Terima kasih!</p>
                     </div>
                 </div>
-                <div className="flex flex-col gap-2 rounded-lg border bg-background p-6 shadow-lg">
-                    <img src="/assets/testi-3.jpg" alt="testi-3" className="h-20 md:h-24 w-full lg:max-w-2xl object-cover rounded-lg" />
-                    <div className="md:ml-6 md:mt-6 flex flex-col items-center md:items-start">
-                        <h3 className="font-bold text-xl md:text-2xl text-tremor-content-strong">Citra</h3>
+                <div className="flex gap-2 rounded-lg border bg-background p-6 shadow-lg">
+                    <img src="/assets/regita.png" alt="testi-1" className="h-12 w-12 object-cover rounded-full" />
+                    <div>
+                        <p className="font-bold text-tremor-content-strong">Regita Rahman</p>
                         <p className="text-tremor-content">Saya sangat senang bisa mengikuti Microsoft Office bersama Soedirman Digital School. Materi yang disampaikan sangat mudah dipahami dan bermanfaat. Terima kasih!</p>
                     </div>
                 </div>
