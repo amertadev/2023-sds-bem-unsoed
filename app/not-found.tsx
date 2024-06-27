cfc746df265996d3fe5810d393b9e3e1a575b836
send0moka <sendomoka2@gmail.com>
2024-06-27 11:41:56 +0700

    feat: Update NotFound component to use SparklesText for displaying "404 Not Found"

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,7 +4,7 @@ export default function NotFound() {
     return (
         <div className="px-6 md:px-40 z-10 w-full flex flex-col justify-center items-center">
             <div className="text-center">
-                <SparklesText text1="404 Not Found" text2="Nantikan update terbaru, Stay tuned!" />
+                <SparklesText text="404 Not Found" />
             </div>
         </div>
     )
